1511a139102dafc45daa9ac11b5a42df603d44b0
anasir514 <anasir514@gmail.com>
2021-08-05 18:22:32 +0100

    fixed SQLAlchemy spelling

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 
 from flask import Flask, render_template
-from flask_sqlalchemy import SQLALchemy
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URL'] = 'sqlite:///test.db' # with 3 /// its relaytive path with 4 its absoluite path
-db = SQLALchemy(app) # initialize databse
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.sqlite3'
+#app.config['SQLALCHEMY_DATABASE_URL'] = 'sqlite:///test.db' # with 3 /// its relaytive path with 4 its absoluite path
+db = SQLAlchemy(app) # initialize databse
 
 @app.route('/')
 def index():
@@ -12,4 +13,3 @@ def index():
 
 if __name__ == "__main__":
     app.run(debug=True)
-    Look
